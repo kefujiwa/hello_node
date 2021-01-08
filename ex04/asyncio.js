a6f547292e840612a8055b3fadef05214478a023
Kenshi Fujiwara <kefujiwa@Kenshis-MacBook-Pro.local>
2021-01-09 00:59:58 +0900

    Impl: ex04~ex05

---
+++ ex04/asyncio.js
@@ -0,0 +1,25 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   asyncio.js                                         :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2021/01/08 23:49:15 by kefujiwa          #+#    #+#             //
+//   Updated: 2021/01/09 00:26:40 by kefujiwa         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+const fs = require('fs');
+
+let total = 0;
+if (process.argv.length == 3)
+{
+	fs.readFile(process.argv[2], function(err, contents) {
+		if (err)
+			console.log(err.message);
+		else
+			total = contents.toString().split('\n').length - 1;
+		console.log(total);
+	});
+}
