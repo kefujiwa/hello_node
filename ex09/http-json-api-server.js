8cdd0e9d05a62e1a7eb38b7cb01f1b6bbc589843
Kenshi Fujiwara <kefujiwa@Kenshis-MacBook-Pro.local>
2021-01-10 21:50:01 +0900

    Fix: change variable names

--- ex09/http-json-api-server.js
+++ ex09/http-json-api-server.js
@@ -6,39 +6,45 @@
 //   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2021/01/09 23:49:29 by kefujiwa          #+#    #+#             //
-//   Updated: 2021/01/10 21:18:10 by kefujiwa         ###   ########.fr       //
+//   Updated: 2021/01/10 21:47:19 by kefujiwa         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 const http = require('http');
-const url = require('url'); //urlの文字列を扱うため
+const url = require('url');
 
 const server = http.createServer(function(req, res) {
 	res.on('error', function(err) {
 		console.error(err.message);
-	}
+	});
 
 	if (req.method == 'GET')
 	{
 		let urlParts = url.parse(req.url, true);
-		let date = Date.parse(urlParts.query.iso);
+		let unixTime = Date.parse(urlParts.query.iso);
 		let resData = '';
 		switch (urlParts.pathname) {
 			case '/api/parsetime':
-				date = new Date(date);
+				let parseTime = new Date(unixTime);
 				resData = JSON.stringify({
-					"hour": date.getUTCHours(),
-					"minute": date.getUTCMinutes(),
-					"second": date.getUTCSeconds()
+					"hour": parseTime.getUTCHours(),
+					"minute": parseTime.getUTCMinutes(),
+					"second": parseTime.getUTCSeconds()
 				});
 				break;
 			case '/api/unixtime':
-				resData = JSON.stringify({"unixtime": date});
+				resData = JSON.stringify({"unixtime": unixTime});
 				break;
 			default:
 				res.writeHead(404, {'Content-Type': 'text/plain'});
 				res.end('Invalid path\n');
 				return;
+		}
+		if (!unixTime)
+		{
+			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.end('Invalid date\n');
+			return;
 		}
 		res.writeHead(200, {'Content-Type': 'application/json'});
 		res.write(resData + '\n');
